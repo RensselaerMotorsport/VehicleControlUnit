b3c9cdb8fcd11d8485871fa0e34b8e3fab958cb4
Tre'Vaughn Barboza <trerese3@gmail.com>
2024-04-12 16:22:51 -0400

    Scheduler Working!

---
+++ includes/ShockPot.h
@@ -0,0 +1,24 @@
+#ifndef RENSSELAERMOTORSPORT_SHOCKPOT_H
+#define RENSSELAERMOTORSPORT_SHOCKPOT_H
+
+#include "AnalogSensor.h"
+
+typedef enum {
+    FRONT_LEFT,
+    FRONT_RIGHT,
+    BACK_LEFT,
+    BACK_RIGHT
+} WHEEL_LOCATION;
+
+typedef struct {
+    AnalogSensor base;
+    float distance;
+    WHEEL_LOCATION wheel_location;
+} ShockPot;
+
+void initShockPot(ShockPot* shockPot, int hz, WHEEL_LOCATION location);
+float getShockPotDistance(ShockPot* shockPot);
+void updateShockPot(void* shockPot);
+float transferFunctionShockPot(float rawVal);
+
+#endif
---
+++ src/Scheduler/Task.c
@@ -0,0 +1,13 @@
+#include "../../includes/Task.h"
+
+void TaskInit(Task* task, Sensor* sensor, int hz) {
+    task->sensor = sensor;
+    task->hz = hz;
+    task->nextExecTime = 0;
+}
+
+void TaskExecute(Task* task) {
+    if (task->sensor && task->sensor->update) {
+        task->sensor->update(task->sensor);
+    }
+}
