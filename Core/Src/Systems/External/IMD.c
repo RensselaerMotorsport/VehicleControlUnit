6665dc34640fc0a68786a9bec89406b24248e1b7
Amar Maksumić <amar.maksumich@gmail.com>
2024-09-28 21:16:54 -0400

    Initial stuff for Lucent

---
+++ Core/Src/Systems/External/IMD.c
@@ -0,0 +1,14 @@
+#include <IMD.h>
+
+// Make sure to update IMD.h as well!!!
+
+/* Things to do
+* 0. Make sure to implement everything needed from updateable type
+* ---> Check Updateable.c and Updateable.h
+* 1. Implement constructor
+* 2. Make template for storing data from IMD
+* 3. Implement getting data from IMD thru CAN
+* ---> CAN needs to be implemented first
+* 4. Tests, check the test folder
+* 5. Figure out the rest~ you can ask for help 
+*/
